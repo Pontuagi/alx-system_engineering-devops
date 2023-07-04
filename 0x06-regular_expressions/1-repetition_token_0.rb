bac4c10dfaacdc1ccf0c88e066afd90fcd11bea2
Pontuagi <benepontuagi@gmail.com>
2023-07-05 02:08:36 +0300

    Repetition token 0

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 # Ruby script that accepts one argument and pass it to a regular expression matching method
 
-regex = /[hb(t{2,5})n]/
+regex = /hbt{2,5}n/
 input = ARGV[0]
 
 matches = input.scan(regex)
-puts matches.join('')
+puts matches.join("\n")
