ec73a2bfa0c0d4cff45ff897b68091b465cdb3bb
Pontuagi <benepontuagi@gmail.com>
2023-07-04 22:56:24 +0300

    Regular expression

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# a Ruby script that accepts one argument and pass it to a regular expression matching method
+
+regex = /School/
+input = ARGV[0]
+
+match = input.match(regex)
+puts match ? match[0] : ''
