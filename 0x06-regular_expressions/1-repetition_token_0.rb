af89c6b18e90925d8c8324f090d357da818a1948
Pontuagi <benepontuagi@gmail.com>
2023-07-04 23:21:11 +0300

    Repetition token

---
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+# Ruby script that accepts one argument and pass it to a regular expression matching method
+
+regex = /[hb(t{2, 5})n]/
+input = ARGV[0]
+
+matches = input.scan(regex)
+puts matches.join('')
