6cb47373780efbc8713c0be81bae8edd00012abb
Pontuagi <benepontuagi@gmail.com>
2023-08-31 05:49:08 +0300

    configure with puppet 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # configure ssh
 exec {'echo':
-  path => 'usr/bin:/bin',
+  path    => 'usr/bin:/bin',
   command => 'echo " IdentityFile ~/.ssh/school\n PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
-  return => [0, 1],
+  return  => [0,1],
 }
