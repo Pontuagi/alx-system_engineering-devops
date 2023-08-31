d8d398f75d3c4af4da6dab75cbe16d55e3f1f329
Pontuagi <benepontuagi@gmail.com>
2023-08-31 05:47:31 +0300

    configure with puppet 2

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,6 @@
 # configure ssh
-file {'/root/.ssh/config':
-  ensure => file,
-  owner => 'ubuntu',
-  group => 'ubuntu',
-  mode => '0600',
-  content => "Host 18.208.120.220\n IdentityFile ~/.ssh/school\n
-  PasswordAuthentication no\n",
+exec {'echo':
+  path => 'usr/bin:/bin',
+  command => 'echo " IdentityFile ~/.ssh/school\n PasswordAuthentication no\n" >> /etc/ssh/ssh_config',
+  return => [0, 1],
 }
