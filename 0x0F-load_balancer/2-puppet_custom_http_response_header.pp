6a8b31ce8d625c33bb018ca58bc7a590227a3b71
Pontuagi <benepontuagi@gmail.com>
2023-09-04 13:52:16 -0700

    puppet header confuguration

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,30 @@
 #!/usr/bin/env bash
 # Configures a brand new ubuntu with a custom HTTP header response
 
-class nginx_custom_header {
-  nginx::resource::location { 'custom_header':
-    location => '~ ^/(.*)$',
-    vhost    => 'default',
-    content  => "add_header X-Served-By $hostname;",
-  }
-}
 
 # Ensure Nginx is installed
 class { 'nginx':
   ensure => 'installed',
 }
+# Define the custom header content
+$custom_header_content = 'add_header X-Served-By $hostname;'
 
-# Ensure Nginx is running
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  subscribe => Nginx::Resource::Location['custom_header'],
+# Define the Nginx configuration file path
+$nginx_config_file = '/etc/nginx/sites-enabled/default'
+
+# Manage the Nginx configuration file
+file { $nginx_config_file:
+  ensure => file,
+  content => $custom_header_content,
+  notify => Exec['configure_custom_header'],
 }
+
+# Run the command to configure the custom header
+exec { 'configure_custom_header':
+  command     => "sed -i '/server {/a \\ \\ \\ \\ ${custom_header_content}' $nginx_config_file && service nginx restart",
+  path        => '/usr/bin',
+  refreshonly => true,
+}
+
+# Notify the exec resource to trigger the custom header configuration
+File[$nginx_config_file] ~> Exec['configure_custom_header']
