6ef14c7f3bdbb92d6a35b307df65ff5b4e466437
Pontuagi <benepontuagi@gmail.com>
2023-09-04 13:35:02 -0700

    Puppet custom header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 # Configures a brand new ubuntu with a custom HTTP header response
 
+class nginx_custom_header {
+  nginx::resource::location { 'custom_header':
+    location => '~ ^/(.*)$',
+    vhost    => 'default',
+    content  => "add_header X-Served-By $hostname;",
+  }
+}
+
 # Ensure Nginx is installed
 class { 'nginx':
   ensure => 'installed',
-}
-
-# Define a custom Nginx location block
-nginx::resource::location { 'custom_header':
-  location => '~ ^/(.*)$',
-  vhost    => 'default',
-  content  => "add_header X-Served-By $hostname;",
 }
 
 # Ensure Nginx is running
