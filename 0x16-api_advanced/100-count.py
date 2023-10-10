7f9deaf6506ef4717537fefaffbc877d52390fa3
Pontuagi <benepontuagi@gmail.com>
2023-10-10 22:11:14 +0300

    count it correction

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,24 +7,13 @@ a recursive function that queries the Reddit API
 import requests
 
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    """
-    Base case: if the subreddit is not valid, or no more posts to fetch, or
-    word_list is empty, return None
-    """
-    if not is_valid_subreddit(subreddit) or not word_list:
-        return None
-
-    # Initialize counts if it's None
-    if counts is None:
-        counts = {}
+def count_words(subreddit, word_list):
+    # Base case: if the subreddit is not valid, return
+    if not is_valid_subreddit(subreddit):
+        return
 
     # Define the Reddit API URL for the subreddit's hot posts
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
-
-    # If there is an 'after' parameter, add it to URL to fetch the next page
-    if after:
-        url += f'&after={after}'
 
     # Set a custom User-Agent header as per Reddit's API guidelines
     headers = {"User-Agent": "MyRedditBot/1.0"}
@@ -39,43 +28,49 @@ def count_words(subreddit, word_list, after=None, counts=None):
 
         # Check if the 'data' dictionary contains 'children' (list of posts)
         if "data" in data and "children" in data["data"]:
+            # Create a dictionary to store word counts
+            word_counts = {}
+
             # Iterate through the posts and parse their titles
             for post in data["data"]["children"]:
-                title = post["data"]["title"].lower()
+                title = post["data"]["title"]
 
-                # Count word occurrences in the title
-                for word in word_list:
-                    if word in counts:
-                        counts[word] += title.count(word)
-                    else:
-                        counts[word] = title.count(word)
+                # Split the title into words and convert to lowercase
+                words = [word.lower() for word in title.split()]
+
+                # Iterate through the word list
+                for keyword in word_list:
+                    keyword = keyword.lower()
+
+                    # Check if the keyword appears in the title's words
+                    if keyword in words:
+                        # Increment the keyword's count in the dictionary
+                        word_counts[keyword] = (
+                            word_counts.get(keyword, 0) + words.count(keyword))
+
+            # Sort the word counts by count (descending)
+            # and then alphabetically (ascending)
+            sorted_counts = sorted(
+                word_counts.items(), key=lambda x: (-x[1], x[0]))
+
+            # Print the sorted counts
+            for keyword, count in sorted_counts:
+                print(f"{keyword}: {count}")
 
             # Check if there are more pages (next page 'after' parameter)
             if "after" in data["data"] and data["data"]["after"]:
                 # Recursively call the function to fetch the next page
-                return count_words(
-                    subreddit, word_list, after=data["data"]["after"],
-                    counts=counts
-                    )
-            else:
-                # Sort and print the counts in descending order by count and
-                # +ascending order by word
-                sorted_counts = sorted(
-                    counts.items(), key=lambda x: (-x[1], x[0]))
-                for word, count in sorted_counts:
-                    print(f"{word}: {count}")
+                count_words(subreddit, word_list)
         else:
-            # If the required data is not present, return None
-            return None
+            # If the required data is not present, return
+            return
     else:
-        # If there was an issue with the request, return None
-        return None
+        # If there was an issue with the request, return
+        return
 
 
 def is_valid_subreddit(subreddit):
-    """
-    A function to check if a subreddit exists by querying its data
-    """
+    # Define a function to check if a subreddit exists by querying its data
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyRedditBot/1.0"}
 
