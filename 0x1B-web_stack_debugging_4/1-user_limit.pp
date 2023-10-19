97f4d158528c4e653f830698778a834b7b2d9637
Pontuagi <benepontuagi@gmail.com>
2023-10-19 23:39:00 +0300

    Holberton user correction

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,11 @@
 # Puppet script to enable login with the holberton user and open a file
 
-user { 'holberton':
-  ensure     => 'present',    # Ensure the user exists
-  managehome => true,      # Create the user's home directory
-  home       => '/home/holberton',  # Specify the home directory path
-  shell      => '/bin/bash',  # Specify the user's default shell
+exec { 'hard-limit-increase':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'soft-limit-increase':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
